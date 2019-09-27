f87924bbc58b57bbe889470ec8318f0c77983c2f
Peter Lebedev <LPeter@mail.ru>
2019-09-27 12:15:07 +0300

    Shortener (5)

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Helper.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task33.task3310;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class Helper {
+
+    public static String generateRandomString() {
+        //final String symbols = "abcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom rnd = new SecureRandom();
+        return new BigInteger(130, rnd).toString(36);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(generateRandomString());
+//        System.out.println(generateRandomString());
+//    }
+}
