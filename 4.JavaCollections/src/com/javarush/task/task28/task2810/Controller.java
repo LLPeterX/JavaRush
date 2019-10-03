b9d14ae1a2520657a93b3ff2ffb53104c0baa646
Peter Lebedev <LPeter@mail.ru>
2019-10-03 13:20:16 +0300

    Aggregator(5)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,15 +1,19 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // класс бизнес-логики
 public class Controller {
 
     private Provider[] providers;
+    private List<Vacancy> vacancies = new ArrayList<>();
 
-    // конструктор с переменным значением аргументов - провадйреро
+    // конструктор с переменным значением аргументов - провадйреров
     public Controller(Provider... providers) {
         if(providers == null || providers.length==0) {
             throw new IllegalArgumentException();
@@ -22,5 +26,13 @@ public class Controller {
         return "Controller{" +
                 "providers=" + Arrays.toString(providers) +
                 '}';
+    }
+
+    public void scan() {
+        // пройтись по всем провайдерам и собрать вакансии
+        for(Provider p : providers) {
+            vacancies.addAll(p.getJavaVacancies(null));
+        }
+        System.out.println(vacancies.size());
     }
 }
