95f4f6d1389c0a774cf9cc79f2f365e3d51ca940
Peter Lebedev <LPeter@mail.ru>
2019-10-03 14:01:27 +0300

    Aggregator(7)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -17,9 +17,12 @@ public class HHStrategy implements Strategy {
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         String url = String.format(URL_FORMAT,"Киев",1);
+        String userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0";
+        String referer = "https://hh.ua/search/vacancy?text=java+%D0%BA%D0%B8%D0%B5%D0%B2";
         Document doc = null;
         try {
-            doc = Jsoup.connect(url).get();
+            //doc = Jsoup.connect(url).get();
+            doc = Jsoup.connect(url).userAgent(userAgent).referrer(referer).get();
             //System.out.println(doc.html());
         } catch (IOException e) {
             e.printStackTrace();
