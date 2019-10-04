2e5c439da54e54db959f0f91dc1ffb522e0e55a4
Peter Lebedev <LPeter@mail.ru>
2019-10-04 17:54:41 +0300

    CashMachine (2)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -2,6 +2,5 @@ package com.javarush.task.task26.task2613;
 
 public class CashMachine {
     public static void main(String[] args) {
-
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,4 +1,19 @@
 package com.javarush.task.task26.task2613;
 
+import java.io.*;
+
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        try {
+            return bis.readLine();
+        } catch (IOException e) {
+            return "";
+        }
+    }
 }
