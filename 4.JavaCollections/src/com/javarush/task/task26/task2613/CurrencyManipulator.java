b5ff45e5cd22e9ef82e1c52e063c5d96a0506113
Peter Lebedev <LPeter@mail.ru>
2019-10-05 15:45:00 +0300

    CashMachine (10)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,7 +1,11 @@
 package com.javarush.task.task26.task2613;
 
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 // класс для конкретной валюты
 public class CurrencyManipulator {
@@ -35,6 +39,51 @@ public class CurrencyManipulator {
 
     public boolean hasMoney() {
         return getTotalAmount()>0;
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return expectedAmount<=getTotalAmount();
+    }
+
+    // выдаь деньги
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        // т.к. при DEPOSIT добавляли значения в любом порядке, надо получить
+        // отсортированную мапу, причем по убыванию номинала
+        Map<Integer, Integer> sorted = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+        sorted.putAll(denominations); // сортируем
+        Map<Integer, Integer> result = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+        if(expectedAmount > getTotalAmount())
+            throw new NotEnoughMoneyException();
+        // проходимся циклом по банкнотам и уменьшаем expectedAmount на сумму выданных банкнот
+        for(Map.Entry<Integer, Integer> entry : sorted.entrySet()) {
+            int denom = entry.getKey();
+            int count = entry.getValue();
+            if(expectedAmount >= denom*count) {
+                // хватает на всё
+                expectedAmount -= denom*count;
+                result.put(denom, count);
+                sorted.put(denom, sorted.get(denom)-count);
+            } else {
+                // вычисляем кол-во нужных банкнот текущего номинала
+                count = expectedAmount/denom;
+                if(count>0) {
+                    result.put(denom, count);
+                    expectedAmount -= denom*count;
+                    sorted.put(denom, sorted.get(denom)-count);
+                }
+            }
+        }
+        if(expectedAmount != 0) {
+            throw new NotEnoughMoneyException();
+        } else {
+            // вычитаем деньги - просто заносим в denominations новые данные из sorted
+            //denominations.replaceAll((BiFunction<? super Integer, ? super Integer, ? extends Integer>) sorted); // ебать IDEA код подставила!
+            // Нет, как говоримл Ленин - "мы пойдем другим путем!"
+            for(Map.Entry<Integer, Integer> entry : sorted.entrySet()) {
+                denominations.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
     }
 
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,10 +1,44 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Map;
 
 class WithdrawCommand implements Command {
     @Override
     public void execute() throws InterruptOperationException {
+        // запросить код валюты и создать для нее манипулятор
+        String kval = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(kval);
+        // в цикле читаем сумму
+        while(true) {
+            ConsoleHelper.writeMessage("Введите сумму: ");
+            try {
+                String strSum = ConsoleHelper.readString();
+                int sum = Integer.parseInt(strSum);
+                if(!manipulator.isAmountAvailable(sum)) {
+                    ConsoleHelper.writeMessage("Недостаточно денег. Повторите ввод");
+                } else {
+                    // выдать пользователю кол-во банкнот
+                    Map<Integer, Integer> moneys = manipulator.withdrawAmount(sum);
+                    // нарисовать их на экране
+                    for(Map.Entry<Integer, Integer> entry : moneys.entrySet()) {
+                        String output = String.format("\t%d - %d",entry.getKey(),entry.getValue());
+                        ConsoleHelper.writeMessage(output);
+                    }
+                    ConsoleHelper.writeMessage("Успешная транзакция");
+                    break;
+                }
 
+            } catch (NumberFormatException e ) {
+                ConsoleHelper.writeMessage("Неверный ввод. Попробуйте снова.");
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("Недостаточно денег в банкомате.");
+            }
+        }
     }
 }
