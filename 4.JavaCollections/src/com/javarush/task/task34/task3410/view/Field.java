e2c2d0a520ff4fd95086903aeb6276abfc4b6360
Peter Lebedev <LPeter@mail.ru>
2019-10-08 14:51:25 +0300

    Sokoban(14)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
     private View view;
@@ -12,7 +15,14 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
-        //this.view.init(); // не сюда!
+        // для Sokoban(14):
+        // 14.3. В конструкторе класса Field:
+        //14.3.1. Создай объект класса KeyHandler.
+        //14.3.2. Добавь его слушателем с помощью метода addKeyListener().
+        //14.3.3. Установи focusable в true (метод setFocusable()).
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     // пока заглушка
@@ -41,5 +51,29 @@ public class Field extends JPanel {
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+    // ------------------- обработчик клавиш ---------------------------------------
+    // вложенный класс (д.б. public, хотя в условии этого нет!)
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case KeyEvent.VK_UP:
+                    eventListener.move(Direction.UP);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case KeyEvent.VK_R:
+                    eventListener.restart();
+                    break;
+            } // switch
+        } // keyPressed()
     }
 }
