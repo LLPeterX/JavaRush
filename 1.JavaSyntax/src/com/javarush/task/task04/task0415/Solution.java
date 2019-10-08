b7f683ff8183e380ff7b6713aecf21c10dc6bb8c
Peter Lebedev <LPeter@mail.ru>
2019-10-08 17:28:04 +0300

    Правило треугольникаSokoban(14)

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -4,19 +4,22 @@ package com.javarush.task.task04.task0415;
 Правило треугольника
 */
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
-        int c = Integer.parseInt(reader.readLine());
-        if(a+b>c || a+c>b || b+c>a) 
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if (a + b > c && a + c > b && b + c > a) {
             System.out.println("Треугольник существует.");
-        else
-            System.out.println( "Треугольник не существует.");
+        } else {
+            System.out.println("Треугольник не существует.");
+        }
     }
 }
