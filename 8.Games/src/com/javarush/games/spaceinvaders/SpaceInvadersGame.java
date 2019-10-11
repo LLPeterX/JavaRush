e12a1ed1206d9acc21e0fad8c57e7d141be12953
Peter Lebedev <LPeter@mail.ru>
2019-10-11 19:51:51 +0300

    space(4)

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,10 +1,16 @@
 package com.javarush.games.spaceinvaders;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+import com.javarush.games.spaceinvaders.gameobjects.Star;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceInvadersGame extends Game  {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private List<Star> stars;
 
     @Override
     public void initialize() {
@@ -14,6 +20,7 @@ public class SpaceInvadersGame extends Game  {
     }
 
     private void createGame() {
+        createStars();
         drawScene();
     }
 
@@ -26,6 +33,17 @@ public class SpaceInvadersGame extends Game  {
         for(int i=0; i<HEIGHT; i++)
             for(int j=0; j<WIDTH; j++)
                 setCellValueEx(i,j, Color.BLACK, "");
+        for(Star star : stars)
+            star.draw(this);
+    }
 
+    private void createStars() {
+        stars = new ArrayList<>();
+        // создать 8 звезд
+        for(int i=0; i<8; i++) {
+            int x = getRandomNumber(WIDTH);
+            int y = getRandomNumber(HEIGHT);
+            stars.add(new Star(x,y)); // ну, не будем проверять совпадение координат
+        }
     }
 }
--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Star.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Star.java
@@ -1,7 +1,18 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.*;
+
 public class Star extends GameObject {
+    private static final String STAR_SIGN = "\u2605";
+
     public Star(double x, double y) {
         super(x, y);
+    }
+
+    // 3. В методе draw(Game) у объекта типа Game должен быть вызван метод setCellValueEx(int, int, Color, String, Color, int)
+    // с параметрами: x, y, Color.NONE, STAR_SIGN, <цвет звезды>, 100.
+    // (<цвет звезды> используй какой тебе нравиться, например, Color.WHITE).
+    public void draw (Game game) {
+       game.setCellValueEx((int)x, (int)y, Color.NONE, STAR_SIGN, Color.YELLOW,100);
     }
 }
