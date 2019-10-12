b1533f915ddccea4a93d20dfa4129c1dd730313e
Peter Lebedev <LPeter@mail.ru>
2019-10-12 11:19:15 +0300

    space(18)

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
@@ -1,7 +1,13 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Ship extends GameObject {
     public boolean isAlive = true;
+    private List<int[][]> frames;
+    private int frameIndex;
 
     public Ship(double x, double y) {
         super(x, y);
@@ -11,6 +17,9 @@ public class Ship extends GameObject {
         // 5. В методе setStaticView(int[][] viewFrame) необходимо вызвать метод setMatrix(int[][]) базового класса.
         // В качестве параметра передай в метод viewFrame.
         super.setMatrix(viewFrame);
+        frames = new ArrayList<int[][]>();
+        frames.add(viewFrame);
+        frameIndex = 0;
     }
 
     public Bullet fire() {
@@ -19,5 +28,12 @@ public class Ship extends GameObject {
 
     public void kill() {
         isAlive = false;
+    }
+
+    public void setAnimatedView(int[][]... viewFrames) {
+        setMatrix(viewFrames[0]);
+        frames =  Arrays.asList(viewFrames);
+        frameIndex = 0;
+
     }
 }
