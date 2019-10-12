2e22579b319ad7ab10d67beacced408b919c124e
Peter Lebedev <LPeter@mail.ru>
2019-10-12 12:49:17 +0300

    space(30)

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
@@ -1,5 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
 public class Boss extends EnemyShip {
@@ -16,5 +17,29 @@ public class Boss extends EnemyShip {
         if(frameCount%10 == 0 || !isAlive)
             super.nextFrame();
 
+    }
+
+    @Override
+    public Bullet fire() {
+        if(!isAlive) return null;
+        // 4. В методе fire() нужно вызвать конструктор класса Bullet с параметрами:
+        // x + 6, y + height, Direction.DOWN, если matrix == ShapeMatrix.BOSS_ANIMATION_FIRST.
+        // 5. В методе fire() нужно вызвать конструктор класса Bullet с параметрами:
+        // x, y + height, Direction.DOWN, если matrix != ShapeMatrix.BOSS_ANIMATION_FIRST.
+        Bullet bullet;
+        if(matrix == ShapeMatrix.BOSS_ANIMATION_FIRST)
+            bullet = new Bullet(x+6, y+height, Direction.DOWN);
+        else
+            bullet = new Bullet(x,y+height,Direction.DOWN);
+        return bullet;
+    }
+
+    @Override
+    public void kill() {
+        if(!isAlive) return;
+        isAlive = false;
+        setAnimatedView(ShapeMatrix.KILL_BOSS_ANIMATION_FIRST,
+                ShapeMatrix.KILL_BOSS_ANIMATION_SECOND,
+                ShapeMatrix.KILL_BOSS_ANIMATION_THIRD);
     }
 }
